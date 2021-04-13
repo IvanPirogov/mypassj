f7a328aeab3c742da61311662db1a0eea62aa6dd
IvanPirogov <pirogov.ivan.p@gmail.com>
2021-04-13 01:05:41 -0500

    comment

--- src/main/java/com/shaman/mypassj/MainApp.java
+++ src/main/java/com/shaman/mypassj/MainApp.java
@@ -1,10 +1,7 @@
 package com.shaman.mypassj;
 
-import com.shaman.mypassj.db.MyPassjGroup;
-import com.shaman.mypassj.db.MyPassjGroups;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.TreeView;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 
@@ -15,14 +12,9 @@ public class MainApp extends Application {
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("/forms/Main.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-
-        TreeView treeViewMain = (TreeView) scene.lookup("#treeViewMain");
-        treeViewMain.setRoot(MyPassjGroups.buildTreeItem());
-        treeViewMain.setEditable(true);
         stage.setScene(scene);
         stage.show();
-
-        MyPassjGroups.buildGroupObjects(treeViewMain.getRoot());
+//        MyPassjGroups.buildGroupObjects(treeViewMain.getRoot());
     }
     public static void main(String[] args) {
         launch(args);
--- src/main/java/com/shaman/mypassj/controllers/MainController.java
+++ src/main/java/com/shaman/mypassj/controllers/MainController.java
@@ -1,22 +1,29 @@
 package com.shaman.mypassj.controllers;
 
+import com.shaman.mypassj.db.DataFile;
+import com.shaman.mypassj.db.MyPassjGroup;
+import com.shaman.mypassj.db.MyPassjGroups;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TreeView;
+import javafx.scene.control.*;
 
 public class MainController {
 
     @FXML
-    private TreeView<?> treeViewMain;
+    private TreeView<MyPassjGroup> treeViewMain;
+
+
+    @FXML
+    private MenuItem addChildContextMenuTreeViewMain;
+
+    @FXML
+    private MenuItem editContextMenuTreeViewMain;
+
+    @FXML
+    private MenuItem deleteContextMenuTreeViewMain;
 
     @FXML
     private MenuBar menuBarMain;
-
-    @FXML
-    private TabPane tabPaneMain;
 
     @FXML
     private Tab tabNotes;
@@ -27,4 +34,75 @@ public class MainController {
     @FXML
     private TableView<?> tableViewLogins;
 
+    private static void changed(ObservableValue<? extends TreeItem<MyPassjGroup>> v, TreeItem<MyPassjGroup> oldValue, TreeItem<MyPassjGroup> newValue) {
+        if (newValue != null) System.out.println(newValue.getValue().getName());
+    }
+
+    @FXML
+    void addChildItem() {
+        if (treeViewMain.getSelectionModel().getSelectedItem() != null) {
+            TreeItem<MyPassjGroup> item = treeViewMain.getSelectionModel().getSelectedItem();
+            MyPassjGroup group = new MyPassjGroup();
+            group.New(DataFile.getIdCounter(), item.getValue().getId(), "New Item", item.getValue().getLevel() + 1, 0);
+            TreeItem<MyPassjGroup> newItem = new TreeItem<>(group);
+            item.getChildren().add(newItem);
+            item.setExpanded(true);
+        }
+    }
+
+    @FXML
+    void addItem() {
+        if (treeViewMain.getSelectionModel().getSelectedItem() != null) {
+            TreeItem<MyPassjGroup> item = treeViewMain.getSelectionModel().getSelectedItem();
+            if (item != treeViewMain.getRoot()) {
+                MyPassjGroup group = new MyPassjGroup();
+                group.New(DataFile.getIdCounter(), item.getValue().getParentid(), "New Item", item.getValue().getLevel(), 0);
+                TreeItem<MyPassjGroup> newItem = new TreeItem<>(group);
+                item.getParent().getChildren().add(newItem);
+            }
+        }
+    }
+
+    @FXML
+    void deleteItem() {
+        if (treeViewMain.getSelectionModel().getSelectedItem() != null) {
+            TreeItem<?> item = treeViewMain.getSelectionModel().getSelectedItem();
+            if (item != treeViewMain.getRoot()) {
+                treeViewMain.getSelectionModel().getSelectedItem().getParent().getChildren().remove(item);
+            }
+        }
+    }
+
+    @FXML
+    void editItem() {
+        if (treeViewMain.getSelectionModel().getSelectedItem() != null) {
+
+        }
+    }
+
+    @FXML
+    void showingPopMenu() {
+        boolean sel = (treeViewMain.getSelectionModel().getSelectedItem() == null);
+        addChildContextMenuTreeViewMain.setDisable(sel);
+        deleteContextMenuTreeViewMain.setDisable(sel);
+        editContextMenuTreeViewMain.setDisable(sel);
+    }
+
+    @FXML
+    void shownPopMenu() {
+
+    }
+
+
+    @FXML
+    public void initialize() {
+        treeViewMain.setRoot(MyPassjGroups.buildTreeItem());
+/*  editItem  --- TreeItem must be String
+        treeViewMain.setCellFactory(TextFieldTreeCell.forTreeView());
+        treeViewMain.setEditable(true);
+ */
+        treeViewMain.getSelectionModel().selectedItemProperty().addListener(MainController::changed);
+
+    }
 }
+
